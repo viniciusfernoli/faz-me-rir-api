4e88aecb875138625314642934ad98b87ff7b2a0
Vinicius Fernandes <vinicius.fernandes@bravolog.com.br>
2024-06-20 17:06:49 -0300

    ajuste cors

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000",".vercel.app"]}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/ativo', methods=['GET'])
 def obter_informacoes_ativo():
